e81ee4150713ac7d3f1826a1ff9e6bf163ed82db
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-10-19 21:53:17 +0600

    📕updated

---
+++ src/data/ContentData.js
@@ -0,0 +1,52 @@
+export const DATA = {
+  metaData: {
+    title: "Web Book",
+    writer: "Akhlak Hossain Jim",
+  },
+  coverPages: [
+    {
+      type: "cover",
+      contentType: "image",
+      src: "https://html-time.vercel.app/cover.webp",
+      alt: "",
+      data: ["$h1$The Web Book", "$h4$by", "$h2$Akhlak Hossain Jim"],
+    },
+    {
+      type: "attribute",
+      data: [
+        "$h1$Akhlak Hossain Jim",
+        "$h4$developed",
+        "$h3$Installable book web app",
+      ],
+    },
+  ],
+  content: [
+    {
+      chapter: "1",
+      chapterName: "",
+      chapterRange: "",
+      pages: [
+        /*
+        use only:
+        $a$ for link
+        $b$...$b$ for bold
+        ^^ for spaces
+        */
+        {
+          pageNo: "1",
+          articles: [
+            `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, aliquam minima! Ipsa molestias laborum minima nesciunt esse reprehenderit totam blanditiis magnam! Aspernatur nostrum rem $b$quidem ad eaque$b$ minus odit $b$rerum fuga$b$ accusamus, nam et, blanditiis consequatur qui eos quis exercitationem ullam eveniet voluptatem ratione iusto recusandae maxime. Ipsum rem, quidem magni perspiciatis incidunt corrupti cum quos unde, accusantium beatae soluta natus eum ex totam pariatur repudiandae numquam, impedit architecto facilis. A recusandae praesentium repudiandae officia, nemo consequatur velit facere, eaque illo nostrum quia ipsam quod provident quaerat. Non unde dolorem fugit maiores nisi eos nam libero deleniti iusto veritatis, totam consequuntur repellat suscipit atque! Hic in optio explicabo ad nesciunt sint quo, repellat asperiores obcaecati ratione iure odit quaerat voluptate nihil vitae voluptatibus fugiat! Culpa recusandae facilis, neque porro laudantium modi, harum officia exercitationem ab hic tempora quas veritatis dolores sequi voluptatem? Facilis vel accusantium praesentium aut? Molestias quas beatae consequuntur, dignissimos, quidem temporibus repellat tempora veniam accusamus, itaque minima illum et dolores! Laborum, laboriosam, optio nisi similique ipsam doloribus ducimus praesentium vitae quasi cum harum velit at voluptas eum natus quidem accusantium ex esse culpa! Consequuntur ut perferendis doloremque eum atque debitis mollitia sapiente? Consequuntur vero labore temporibus et.`,
+            `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, aliquam minima! Ipsa molestias laborum minima nesciunt esse reprehenderit totam blanditiis magnam! Aspernatur nostrum rem quidem ad eaque minus odit rerum fuga accusamus, nam et, blanditiis consequatur qui eos quis exercitationem ullam eveniet voluptatem ratione iusto recusandae maxime. Ipsum rem, quidem magni perspiciatis incidunt corrupti cum quos unde, accusantium beatae soluta natus eum ex totam pariatur repudiandae numquam, impedit architecto facilis. A recusandae praesentium repudiandae officia, nemo consequatur velit facere, eaque illo nostrum quia ipsam quod provident quaerat. Non unde dolorem fugit maiores nisi eos nam libero deleniti iusto veritatis, totam consequuntur repellat suscipit atque! Hic in optio explicabo ad nesciunt sint quo, repellat asperiores obcaecati ratione iure odit quaerat voluptate nihil vitae voluptatibus fugiat! Culpa recusandae facilis, neque porro laudantium modi, harum officia exercitationem ab hic tempora quas veritatis dolores sequi voluptatem? Facilis vel accusantium praesentium aut? Molestias quas beatae consequuntur, dignissimos, quidem temporibus repellat tempora veniam accusamus, itaque minima illum et dolores! Laborum, laboriosam, optio nisi similique ipsam doloribus ducimus praesentium vitae quasi cum harum velit at voluptas eum natus quidem accusantium ex esse culpa! Consequuntur ut perferendis doloremque eum atque debitis mollitia sapiente? Consequuntur vero labore temporibus et.`,
+            `^^Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, aliquam minima! Ipsa molestias laborum minima nesciunt esse reprehenderit totam blanditiis magnam! Aspernatur nostrum rem quidem ad eaque minus odit rerum fuga accusamus, nam et, blanditiis consequatur qui eos quis exercitationem ullam eveniet voluptatem ratione iusto recusandae maxime. Ipsum rem, quidem magni perspiciatis incidunt corrupti cum quos unde, accusantium beatae soluta natus eum ex totam pariatur repudiandae numquam, impedit architecto facilis. A recusandae praesentium repudiandae officia, nemo consequatur velit facere, eaque illo nostrum quia ipsam quod provident quaerat. Non unde dolorem fugit maiores nisi eos nam libero deleniti iusto veritatis, totam consequuntur repellat suscipit atque! Hic in optio explicabo ad nesciunt sint quo, repellat asperiores obcaecati ratione iure odit quaerat voluptate nihil vitae voluptatibus fugiat! Culpa recusandae facilis, neque porro laudantium modi, harum officia exercitationem ab hic tempora quas veritatis dolores sequi voluptatem? Facilis vel accusantium praesentium aut? Molestias quas beatae consequuntur, dignissimos, quidem temporibus repellat tempora veniam accusamus, itaque minima illum et dolores! Laborum, laboriosam, optio nisi similique ipsam doloribus ducimus praesentium vitae quasi cum harum velit at voluptas eum natus quidem accusantium ex esse culpa! Consequuntur ut perferendis doloremque eum atque debitis mollitia sapiente? Consequuntur vero labore temporibus et.`,
+            "$b$skalsl$b$",
+          ],
+        },
+      ],
+    }, // page1
+  ],
+};
+
+// {
+//   page: "",
+//   articles: [],
+// },
